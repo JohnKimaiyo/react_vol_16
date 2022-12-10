0e02da560866155364a5c921cede9403cbe85e4c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-10 04:29:39 +0300

    change name usestate

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Greet from "./component/Greet";
 import Welcome from "./component/Welcome";
 import Hello from "./component/Hello";
 import Title from "./component/Title";
+import Myname from "./component/Myname";
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
       <Hello />
       <Title name="Frontend Dveloepr" />
       <Title name="Soldity Developer" />
+      <Myname/>
     </div>
   );
 }
